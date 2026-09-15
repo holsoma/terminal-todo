e08edf08793258f09931e9fe40ffe1e398d0bec6
holsoma <amosloh1911@gmail.com>
2026-09-15 18:27:41 +0800

    Update TeaVM JSExport annotation

--- src/main/java/todo/Main.java
+++ src/main/java/todo/Main.java
@@ -6,6 +6,6 @@ public final class Main {
     private static final TodoApp APP = new TodoApp();
     private Main() {}
     public static TodoApp createApp() { return new TodoApp(); }
-    @JSExport("execute")
+    @JSExport
     public static String execute(String command) { return APP.execute(command); }
 }
